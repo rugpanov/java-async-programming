f94982a4bba2ec9472abc98788aa6732aea9c499
grigory.panov <gripanov@gmail.com>
2024-03-17 23:54:33 +0100

    Add Benchmarks for Virtual and Platform Threads
    It's easy to run 100_000 Virtual Threads, it takes only 303ms to start them.
    
    It's only possible to run 16340 platform threads, it takes 2711ms to start them.

--- src/main/java/dev/grigri/Main.java
+++ src/main/java/dev/grigri/Main.java
@@ -1,10 +1,23 @@
 package dev.grigri;
 
-import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        ServerNoAsync server = new ServerNoAsync();
-        server.run();
+    public static void main(String[] args) {
+        Instant start = Instant.now();
+        ThreadsBenchmarksUtils.startManyVirtualThreads(100_000);
+        Instant finish = Instant.now();
+        long timeElapsed = Duration.between(start, finish).toMillis();
+        System.out.println("Time elapsed to start virtual threads: " + timeElapsed + "ms");
+
+        start = Instant.now();
+        try {
+            ThreadsBenchmarksUtils.startManyThreads(100_000);
+        } catch (OutOfMemoryError e) {
+            finish = Instant.now();
+            timeElapsed = Duration.between(start, finish).toMillis();
+            System.out.println("Time elapsed to start virtual threads: " + timeElapsed + "ms");
+        }
     }
 }
---
+++ src/main/java/dev/grigri/ThreadsBenchmarksUtils.java
@@ -0,0 +1,29 @@
+package dev.grigri;
+
+public class ThreadsBenchmarksUtils {
+    public static void startManyThreads(int nThreads) {
+        for (int i = 0; i < nThreads; i++) {
+            System.out.println("Starting thread " + i);
+            new Thread(() -> {
+                try {
+                    Thread.sleep(10_000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+    }
+
+    public static void startManyVirtualThreads(int nThreads) {
+        for (int i = 0; i < nThreads; i++) {
+            System.out.println("Starting thread " + i);
+            Thread.ofVirtual().start(() -> {
+                try {
+                    Thread.sleep(10_000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+    }
+}
